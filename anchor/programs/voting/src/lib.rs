79d5d8eabef8fdb89552b47028c512e130cd8ff0
psatomas <psatomas@gmail.com>
2025-03-23 19:07:11 -0300

    feat: updated libs.rs

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -7,4 +7,8 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 #[program]
 pub mod voting{
     use super::*;
+
+    pub fn initialize_poll(ctx: Context<InitializePoll>, _poll_id: u64 ) -> ProgramResult {
+       Ok(()) 
+    }
 }
